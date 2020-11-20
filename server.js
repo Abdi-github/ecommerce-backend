b05fce0ad0b187496dc7af933e6a55ae85d8a802
abdigit <abdulkadirahmed1985@gmail.com>
2020-11-20 06:45:55 +0100

    Backend server setup

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import nodemon from "nodemon";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+// mongoDB connection
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.psqzr.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Database Connected");
+  });
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is running.",
+  });
+});
+app.post("/data", (req, res) => {
+  res.status(200).json({
+    message: req.body,
+  });
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Serve at http://localhost:${process.env.PORT}`);
+});
